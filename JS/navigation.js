5c9be0765051bbec329e34af23b3bf28ef402bf4
BitThzn <adryanoliveiratol@gmail.com>
2026-06-22 16:57:34 -0300

    v1.5.0

--- JS/navigation.js
+++ JS/navigation.js
@@ -12,10 +12,6 @@ function openGithub() {
     window.open("https://github.com/BitThzn", "_blank");
 }
 
-function openAbout() {
-    window.location.href = "about.html";
-}
-
 function openIdentity() {
     window.location.href = "identity.html";
 }
@@ -24,12 +20,20 @@ function openGhostRepo() {
     window.location.href = "./projects_pages/system-ghost.html";
 }
 
+function openDashboard() {
+    window.location.href = "./projects_pages/system-dashboard.html";
+}
+
 function openComingSoon(projectName) {
     window.location.href = `coming.html?project=${projectName}`;
 }
 
 function openSystemGhostRepo() {
     window.open("https://github.com/BitThzn/SYSTEM-GHOST", "_blank");
+}
+
+function openSystemDashboardRepo() {
+    window.open("https://github.com/BitThzn/SYSTEM-DASHBOARD", "_blank");
 }
 
 function returnToCore() {
